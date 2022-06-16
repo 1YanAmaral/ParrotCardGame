2d33718084bfc754b94d7569ef606ae0c1d1bc2c
Yan <yanc.amaral@gmail.com>
2022-06-16 12:38:17 -0300

    efeito flip

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-let cards = prompt("Com quantas cartas quer jogar?");
+let cards = prompt("Com quantas cartas quer jogar? Escolha entre 4 a 14 cartas:");
 
-    while(cards%2 !== 0) {
-        cards = prompt("Com quantas cartas quer jogar?");
+
+if(cards%2 !== 0 || cards < 4 || cards > 14) {
+   alert("Escolha um número válido de cartas!");
+   cards = prompt("Com quantas cartas quer jogar? Escolha entre 4 a 14 cartas:");
+    
+}
+
+
+
+
+let cardList = [];
+
+
+for (let i = 0; i < cards; i++) {
+        document.querySelector(".container").innerHTML += 
+        `<div class="card" onclick="select(this)">
+        <div class="front-face face">
+          <img src="imgs/front.png" alt="">
+        </div>
+        <div class="back-face face">
+          <img src="imgs/metalparrot.gif" alt="">
+        </div>
+    </div>`;
     }
 
-let cardList =[];
-cardList.length = cards;
-let cont =0;
-while(cont < cards) {
-    document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + `<div class="card"><img src="imgs/front.png" alt="">
-    </div>`;
-    cont++;
+
+
+function select (element) {
+  element.classList.toggle("flip");
 }
+   
