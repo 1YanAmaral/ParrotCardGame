24f0f11878b5f056f1005e6bef90f808d4807057
Yan <yanc.amaral@gmail.com>
2022-06-17 12:17:35 -0300

    cartas iguais viradas para cima e cartas diferentes viradas para baixo

--- script.js
+++ script.js
@@ -5,43 +5,56 @@ verify(cards);
 function verify() {
   if(cards%2 !== 0 || cards < 4 || cards > 14 || isNaN(cards)) {
     alert("Escolha um número válido de cartas!");
-    cards = prompt("Com quantas cartas quer jogar? Escolha entre 4 a 14 cartas:");
+    cards = prompt("Com quantas cartas quer jogar? Escolha um número par entre 4 a 14 cartas:");
     verify(cards); 
  }
 }
 
-
 let cardList = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
-//cardList = cardList.sort(comparador);
 
 let cardsPlaying =[];
 
 for (let i = 0; i < cards/2; i++) {
        
-  cardsPlaying.push(`<div class="card" onclick="select(this)">
+  cardsPlaying.push(`<div class="card" onclick="select(this); lastElement(this)">
   <div class="front-face face"><img src="imgs/front.png" alt=""></div>
   <div class="back-face face"><img src="imgs/${cardList[i]}" alt=""></div></div>`);
   
-  cardsPlaying.push(`<div class="card" onclick="select(this)">
+  cardsPlaying.push(`<div class="card" onclick="select(this); lastElement(this)">
   <div class="front-face face"><img src="imgs/front.png" alt=""></div>
   <div class="back-face face"><img src="imgs/${cardList[i]}" alt=""></div></div>`);
  
-  }
-cardsPlaying = cardsPlaying.sort(comparador);
+  cardsPlaying = cardsPlaying.sort(comparador);
+}
 
 
 document.querySelector(".container").innerHTML = cardsPlaying;
 
-console.log(cardsPlaying);
+let compare = [];
+let lastSelected;
 
-
-
-
-
+function lastElement(element) {
+  lastSelected = element;
+}
 
 function select (element) {
-  element.classList.toggle("flip");
+  element.classList.add("flip");
+  let item = element.querySelector(".back-face").innerHTML;
+  compare.push(item);
+  if(compare.length > 1) {
+    if(compare[0] !== compare[1]) {
+      alert('nope');
+      element.classList.remove("flip");
+      lastSelected.classList.remove("flip");
+      compare = [];
+    } else {
+      compare = [];
+    }
+  }
 }
+
+
+
 
 function comparador() { 
 	return Math.random() - 0.5; 
