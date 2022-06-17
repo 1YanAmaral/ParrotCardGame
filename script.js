227ff194cce10b217beb52611068ea2f6ac1da78
Yan <yanc.amaral@gmail.com>
2022-06-17 10:18:41 -0300

    verifica número de cartas e tabuleiro aleatório

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-let cards = prompt("Com quantas cartas quer jogar? Escolha entre 4 a 14 cartas:");
+let cards = prompt("Com quantas cartas quer jogar? Escolha um número par entre 4 e 14 cartas:");
 
+verify(cards);
 
-if(cards%2 !== 0 || cards < 4 || cards > 14) {
-   alert("Escolha um número válido de cartas!");
-   cards = prompt("Com quantas cartas quer jogar? Escolha entre 4 a 14 cartas:");
-    
+function verify() {
+  if(cards%2 !== 0 || cards < 4 || cards > 14 || isNaN(cards)) {
+    alert("Escolha um número válido de cartas!");
+    cards = prompt("Com quantas cartas quer jogar? Escolha entre 4 a 14 cartas:");
+    verify(cards); 
+ }
 }
 
 
-
-
 let cardList = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
-cardList.sort(comparador);
+//cardList = cardList.sort(comparador);
+
+let cardsPlaying =[];
+
+for (let i = 0; i < cards/2; i++) {
+       
+  cardsPlaying.push(`<div class="card" onclick="select(this)">
+  <div class="front-face face"><img src="imgs/front.png" alt=""></div>
+  <div class="back-face face"><img src="imgs/${cardList[i]}" alt=""></div></div>`);
+  
+  cardsPlaying.push(`<div class="card" onclick="select(this)">
+  <div class="front-face face"><img src="imgs/front.png" alt=""></div>
+  <div class="back-face face"><img src="imgs/${cardList[i]}" alt=""></div></div>`);
+ 
+  }
+cardsPlaying = cardsPlaying.sort(comparador);
+
+
+document.querySelector(".container").innerHTML = cardsPlaying;
+
+console.log(cardsPlaying);
 
 
 
-for (let i = 0; i < cards; i++) {
-        document.querySelector(".container").innerHTML += 
-        `<div class="card" onclick="select(this)">
-        <div class="front-face face">
-          <img src="imgs/front.png" alt="">
-        </div>
-        <div class="back-face face">
-          <img src="imgs/${cardList[i]}" alt="">
-        </div>
-    </div>`;
-    }
 
 
 
