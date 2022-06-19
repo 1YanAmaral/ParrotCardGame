e00ac5a03f6659e33d77b68fef1938fce3de3917
Yan <yanc.amaral@gmail.com>
2022-06-19 13:38:06 -0300

    bonus implementados

--- script.js
+++ script.js
@@ -33,11 +33,22 @@ document.querySelector(".container").innerHTML = cardsPlaying.join(' ');
 let compare = [];
 let lastSelected;
 let playCount = 0;
-let gameOver = 0;
-let array = [];
+let cardsFlipped = [];
 
 function lastElement(element) {
   lastSelected = element;
+}
+
+function removeFlip(element) {
+  element.classList.remove("flip");
+}
+
+function removeClick(element) {
+  element.removeAttribute("onclick");
+}
+
+function addClick(element) {
+  element.setAttribute("onclick", "select(this); lastElement(this)");
 }
 
 function select (element) {
@@ -46,24 +57,51 @@ function select (element) {
   compare.push(item);
   playCount++;
   if(compare.length > 1) {
+    
     if(compare[0] !== compare[1]) {
-      alert('nope');
-      element.classList.remove("flip");
-      lastSelected.classList.remove("flip");
+      setTimeout(removeFlip, 1000, element);
+      setTimeout(removeFlip, 1000, lastSelected);
       compare = [];
     } else {
       compare = [];
     }
+    
   }
-  array = Array.from(document.querySelectorAll(".flip"));
-  console.log(array.length);
-  if (array.length === cardsPlaying.length) {
-    alert(`Você ganhou em ${playCount} jogadas!`);
-  } 
+  cardsFlipped = Array.from(document.querySelectorAll(".flip"));
+  if (cardsFlipped.length === cardsPlaying.length) {
+    setTimeout(alert, 300, `Você ganhou em ${playCount} jogadas em ${min} minutos e ${sec} segundos!`);  
+    setTimeout(playAgain, 300);
+    clearInterval(idInterval);
+  }
+  
+} 
+
+
+function playAgain() {
+  let choice = prompt("Quer jogar de novo? Responda com sim ou não:");
+  if (choice === "sim") {
+    document.location.reload(true);
+  } else {
+    alert("Obrigado por jogar!");
+    clearInterval(idInterval);
+  }
 }
 
 
+let sec = 0;
+let min = 0;
 
+function timer() {
+  sec++;
+  if(sec == 60) {
+    min++;
+    sec = 0;
+    document.querySelector(".min").innerHTML = min;
+}
+  document.querySelector(".sec").innerHTML = sec;
+}
+
+const idInterval = setInterval(timer, 1000);
 
 function comparador() { 
 	return Math.random() - 0.5; 
